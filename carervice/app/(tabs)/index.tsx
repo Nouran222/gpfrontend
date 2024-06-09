89e8141edef15f9ddc39f6653ca5f504f962370f
yasmeen-mohy <130379431+yasmeen-mohy@users.noreply.github.com>
2024-06-09 13:13:33 +0300

    splashscreen image

--- carervice/app/(tabs)/index.tsx
+++ carervice/app/(tabs)/index.tsx
@@ -2,11 +2,11 @@ import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import ConsumerLogin from '../../Screens/ConsumerLoginScreen.js'
 import ConsumerRegister from '../../Screens/Registeration/ConsumerRegisterScreen.js'
 import ProviderRegister from '../../Screens/Registeration/ProviderRegisterScreen.js'
-
+import SplashScreen from '../../Screens/SplashScreens/SplashScreen.js'
 export default function HomeScreen() {
   
   return (
-    <ProviderRegister/>
+    <SplashScreen/>
   );
 }
 
