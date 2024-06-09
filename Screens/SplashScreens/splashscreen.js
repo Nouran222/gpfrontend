fdfa4502cd11d533a3522d70c0373d139d309367
Nouran222 <nouransayednasr@gmail.com>
2024-06-09 20:16:56 +0300

    animation

--- Screens/SplashScreens/splashscreen.js
+++ Screens/SplashScreens/splashscreen.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Button, Image, StyleSheet, View } from "react-native";
 import PrimaryBtn from "..//../components/PrimaryBtn";
+import LottieView from 'lottie-react-native';
 
-const SplashScreen = ({navigation}) => {
+const SplashScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
+
       <Image
         source={require("../../assets/images/splashscreen.jpg")}
         style={styles.image}
       />
+      <View style={styles.tow}>
+        <LottieView style={{ flex: 1 }} source={require('../../assets/animations/tow.json')} autoPlay loop />
+      </View>
+
       <View style={styles.buttonsContainer}>
         <View style={styles.btn}>
           <PrimaryBtn
@@ -35,23 +41,31 @@ const SplashScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
+    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "rgb(251, 245, 247)",
+    padding: 10,
   },
   image: {
     width: "90%",
-    height: "70%",
+    height: "20%",
     resizeMode: "contain",
   },
   buttonsContainer: {
     flexDirection: "row",
+    marginTop: 10,
   },
   btn: {
     flex: 1,
     marginHorizontal: 10,
     // backgroundColor: "red"
   },
+  tow: {
+    // height:300,
+    width: '100%',
+    aspectRatio: 1,
+    flexShrink: 1
+  }
 });
 
 export default SplashScreen;
