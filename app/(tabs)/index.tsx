74f814a0f8b1619b0a05dc570914a33fefcc4179
HassanWalid <hassan.walid77@gmail.com>
2024-06-25 03:21:07 +0300

    hassan

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,9 +4,10 @@ import { MyStack } from "../../Navigations/StackNavigation.js";
 import { useFonts } from "expo-font";
 import LoadingScreen from "../../Screens/SplashScreens/loadingScreen.js";
 import Splashscreen from "@/Screens/SplashScreens/splashscreen.js";
-// import {MyTabs} from "../../Navigations/ProviderTopTabsNavigator/BottomTabsNavigation.js"
-import { MyTabs } from "../../Navigations/ProviderTopTabsNavigator/TopTabsNavigation.js";
+import { MyTabs } from "../../Navigations/ProviderTopTabsNavigator/BottomTabsNavigation.js";
+// import { MyTabs } from "../../Navigations/ProviderTopTabsNavigator/TopTabsNavigation.js";
 import ProviderRegisterScreen from "@/Screens/Registeration/ProviderRegisterScreen.js";
+import ProviderHomeScreen from "@/Screens/ProviderScreens/ProviderHomeScreen.js";
 
 const fonts = {
   Oswald: require("../../assets/fonts/static/Oswald-Bold.ttf"),
@@ -15,20 +16,20 @@ const fonts = {
 export default function HomeScreen() {
   const [fontsLoaded, loadFonts] = useFonts(fonts);
 
-  if (fontsLoaded) {
-    return (
-      <>
-        <MyStack></MyStack>
-      </>
-    );
-  }
+  // if (fontsLoaded) {
+  //   return (
+  //     <>
+  //       <MyStack></MyStack>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
-      {/* <MyTabs /> */}
-      <LoadingScreen />
-      <Splashscreen />
-      <MyStack></MyStack>
+      <MyTabs />
+      {/* <LoadingScreen /> */}
+      {/* <Splashscreen /> */}
+      {/* <MyStack></MyStack> */}
       {/* <ProviderRegisterScreen></ProviderRegisterScreen> */}
     </>
   );
